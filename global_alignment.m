620f336c0fc97f9e927d351c3ce112c197c23137
Alaric <yxw1242@case.edu>
2018-10-11 20:38:54 -0400

    Added the dynamic way of counting the number of optimal alignments

--- global_alignment.m
+++ global_alignment.m
@@ -2,22 +2,36 @@ function [score,number_of_op_solu,op_align] = global_alignment(sequence1,sequenc
     number_of_op_solu = 1;
     op_align = 1;
     global table_m 
+    global table_n
     global path_list
     J = length(sequence1) + 1;
     I = length(sequence2) + 1;
+    table_n = nan*ones(I,J);
+    for i = 1:I
+            table_n(i,1) = 1;
+        end
+    for j = 1:J
+            table_n(1,j) = 1;
+        end
     score = D_g(I,J,sequence1,sequence2, score_m, score_s, score_d);
     path_list = [];
     find_Path(I,J,[],sequence1,sequence2, score_m, score_s, score_d);
     table_m
-    number_of_op_solu = length(path_list);
+    optimal_score = score
+    number_of_op_solu = length(path_list); 
+    %number_of_op_solu = table_n(I,J); %they are equivalent, this line is more efficient.
+    fprintf("The number of optimal alignment calculated by recursive search path after the table is built: %d\n", number_of_op_solu);
+    fprintf("The number of optimal alignment calculated by dynamic programming when the table is building: %d\n", table_n(I,J));
     for i = 1: number_of_op_solu
         [ali1,ali2] = trace_opt_ali('g',path_list{i},sequence1,sequence2)
     end
-    score
+    
 end
 
 function score = D_g(i,j,sequence1,sequence2, score_m, score_s, score_d)
     global table_m 
+    global path_list
+    global table_n
     if(isnan(table_m(i-1,j-1)))
         table_m(i-1,j-1) = D_g(i-1,j-1,sequence1,sequence2, score_m, score_s, score_d);
     end
@@ -35,8 +49,24 @@ function score = D_g(i,j,sequence1,sequence2, score_m, score_s, score_d)
     if(isnan(table_m(i-1,j)))
         table_m(i-1,j) = D_g(i-1,j,sequence1,sequence2, score_m, score_s, score_d);
     end
-    score = max([table_m(i-1,j-1)+step_score , table_m(i-1,j)+ score_d,table_m(i,j-1) + score_d]);
+    diag = table_m(i-1,j-1)+step_score;
+    up = table_m(i-1,j)+ score_d;
+    left = table_m(i,j-1) + score_d;
+    score = max([diag,up,left]);
     table_m(i,j) = score;
+    count_path_diag = 0;
+    count_path_up = 0;
+    count_path_left = 0;
+    if(diag == score)
+        count_path_diag = table_n(i-1,j-1);
+    end
+    if(up == score)
+        count_path_up = table_n(i-1,j);
+    end
+    if(left == score)
+        count_path_left = table_n(i,j-1);
+    end
+    table_n(i,j) = count_path_diag + count_path_left + count_path_up;
 
 end
 
